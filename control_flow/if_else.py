c0d78f96b2d87d4455ef3433d131476815d907c0
rania-codes4 <raniasingh424@gmail.com>
2026-01-29 18:49:08 +0530

    Add if-else examples

---
+++ control_flow/if_else.py
@@ -0,0 +1,16 @@
+# If-else examples
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+# Even or odd check
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
